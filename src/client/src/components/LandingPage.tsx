9a8204d3b4afb1f04f86aaa721632800e3942aaa
YaHeng Su <yaheng.su1@gmail.com>
2019-09-15 23:39:25 -0400

    added landing page and some files

---
+++ src/client/src/components/LandingPage.tsx
@@ -0,0 +1,15 @@
+
+
+import React, { Component } from 'react'
+
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>
+                    To start, create a new trip or enter your itinerary code.
+                </h2>
+            </div>
+        )
+    }
+}
