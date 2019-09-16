d3d9e1146e50dc182aa92bf312300db9e0258f49
YaHeng Su <yaheng.su1@gmail.com>
2019-09-16 00:32:02 -0400

    boilerplate

---
+++ src/client/src/components/Itinerary.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Itinerary extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
